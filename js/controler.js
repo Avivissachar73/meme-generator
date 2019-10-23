3ffc0fcfb3e190984839cb2938e2b606bb6f19cc
Aviv Issachar <Avivissachar73@gmail.com>
2019-10-23 21:57:19 +0300

    fix prevent default bug

--- js/controler.js
+++ js/controler.js
@@ -72,7 +72,7 @@ function canvasClicked(event) {
     //     selectTxt(getCurrTxtIdx());
     //     return;
     // }
-    event.preventDepault();
+    // event.preventDepault();
     if (!onChangeCurrTxtIdx(event)) {
         console.log('wowowow');
         resetCurrTxtIdx();
